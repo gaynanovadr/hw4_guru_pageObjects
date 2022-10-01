f96c4cac08a06349665843913e3c3943ca6702ea
dinara <gaynanovadr@mail.ru>
2022-10-01 15:38:18 +0200

    added Data generated with Faker and Random in separate class

--- src/test/java/com/demoqa/tests/AuthorizationFormTest.java
+++ src/test/java/com/demoqa/tests/AuthorizationFormTest.java
@@ -2,35 +2,14 @@ package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.demoqa.pages.RegistrationFormPage;
-import com.demoqa.utils.Month;
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Locale;
+import static com.demoqa.testData.UserInfo.*;
 
 public class AuthorizationFormTest {
 
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
-
-    Faker faker = new Faker(new Locale("de"));
-
-    String firstName = faker.name().firstName();
-    String lastName = faker.name().lastName();
-    String email = faker.internet().safeEmailAddress();
-    String number = String.valueOf(faker.phoneNumber().subscriberNumber(10));
-    String gender = faker.demographic().sex();
-    String pic = "pic.png";
-    String hobby = "Sports";
-    String subject = "English";
-    String path = "src/test/resources/pic.png";
-    String address = String.valueOf(faker.address().fullAddress());
-    String state = "NCR";
-    String city = "Noida";
-    String day = String.valueOf(faker.number().numberBetween(1, 31));
-    String month = String.valueOf(Month.getRandomMonth());
-    String year = String.valueOf(faker.number().numberBetween(1950, 2010));
-    String date = day + " " + month + "," + year;
 
     @BeforeAll
     static void setUp() {
